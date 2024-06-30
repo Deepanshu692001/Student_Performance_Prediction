32f93123cac39eacb5c73ab841ecf92a51cf6069
Deepanshu Gupta <deepanshugupta06092001@gmail.com>
2024-07-01 04:00:48 +0530

    Update setup.py with new package dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-    requirement=[]
+    requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     
-    return requirement
+    return requirements
 
 setup(
 name='mlproject',
@@ -23,7 +23,6 @@ version='0.0.1',
 author='Deepanshu',
 author_email='deepanshugupta06092001@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirement.txt')
+install_requires=get_requirements('requirements.txt')
 
 )
->>
